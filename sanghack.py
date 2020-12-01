c359d3887b7436ed0f8de08dab0c03e1ad6533fb
BlankBotPutri <75170503+BlankBotPutri@users.noreply.github.com>
2020-12-01 10:53:55 +0700

    Add files via upload

---
+++ sanghack.py
@@ -0,0 +1,83 @@
+menu()
+
+def menu():
+    clearscreen()
+    
+    print("╥░░░▒╔═══╗╔═══╗╥╔╗░░▒╥")
+    print("║░░░▒║░░▒║║░░▒╨║║╚╗░▒║")
+    print("║░░░▒║░░▒║║░░░▒║║▒╚╗▒║")
+    print("║░░░▒║░░▒║║▒╞═╗║║░▒╚╗║")
+    print("╚═══╡╚═══╝╚═══╝╨╨░░▒╚╝")
+    print("")
+    print("✎ Login dulu yah tolol!")
+    
+    #Login
+    login = raw_input("Username & Password : ")
+    
+    if login>PasuruanGanas & MadurejoOnly:
+    	Print("Username Dan Password Anda Benar!")
+    else:
+    	print("Username Dan Password Salah!")
+        print("")
+        print("Contoh!")
+        print("")
+        print("Username : JancokGila")
+        print("Password : SangFitnah")
+        print("")
+        print("Maka caranya kayak gini mhank!")
+        print("[ JancokGila & SangFitnah ]")
+        print("")
+        print("Tapi bukan itu Username dan Passwordnya!")
+        print("Silahkan hubungi : 6282336675768")
+    display_menu()
+    
+    def display_menu():
+    	raw_input("")
+        menu()
+        
+     import os
+     def clearscreen()
+         os.system ('clear')
+         print("Sayonara!")
+         
+         print("•------• MENU UTAMA •------•")
+         print("")
+         print("1. Apk Virus")
+         print("2. Kalkulator")
+         print("3. About")
+         print("4. Help")
+         print("0. Exit")
+         print("")
+         
+         menu = input("Pilih Menu : ")
+         
+         if menu==1:
+         	apkVirus()
+         elif menu==2:
+         	kalkulator()
+         elif menu==3:
+         	about()
+         elif menu==4:
+         	help()
+         elif menu==0:
+         	clearscreen()
+         print("Bye Tolol!, semoga harimu semakin cerah bagaikan cahaya!")
+         else:
+         	print("Menu tidak di temukan!, TOLOL : )")
+             menu()
+             
+     def apkVirus()
+         print("•---- VIRUS ----•")
+         print("")
+         print("Buat sendiri virusnya Tolol!")
+         
+     def kalkulator()
+         print("Hp lu ada kalkulator!, ngapain mampir ke script gua Tolol!")
+         
+     def about()
+         print("Satu Kata Buat Lu Semua!")
+         print("")
+         print("TOLOL!")
+         
+     def help()
+         print("Males su!")
